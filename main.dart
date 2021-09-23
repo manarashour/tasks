1717729d833b80bf604edb07b04fed6a18c8ab6a
manarashour <55696678+manarashour@users.noreply.github.com>
2021-09-24 00:06:28 +0200

    Add files via upload
    image

--- main.dart
+++ main.dart
@@ -1,84 +1,21 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 
+import 'home.dart';
+
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.pink[100],
-        appBar: AppBar(
-          title: Text('The Dice App',
-              style: TextStyle(
-                color: Color(0xff030303),
-                fontWeight: FontWeight.bold,
-              )
-          ),
-          backgroundColor: Colors.pink[200],
-          centerTitle: true,
-        ),
-        body: DicePage(),
-      ),
-    ),
-  );
+  runApp(MyApp());
 }
 
-class DicePage extends StatefulWidget {
-  @override
-  _DicePageState createState() => _DicePageState();
-}
-
-class _DicePageState extends State<DicePage> {
-  int leftDiceNumber = 1;
-  int rightDiceNumber = 1;
-
-  void changeDiceFace() {
-    setState(() {
-      leftDiceNumber = Random().nextInt(6) + 1;
-      rightDiceNumber = Random().nextInt(6) + 1;
-    });
-  }
-
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          RichText(
-            textScaleFactor : 9.0,
-            text: TextSpan(
-              text: '${leftDiceNumber + rightDiceNumber}',
-              style: DefaultTextStyle.of(context).style,
-            ),
-          ),
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Image.asset(
-                  'images/dice$leftDiceNumber.png',
-                ),
-              ),
-              //Get students to create the second die as a challenge
-              Expanded(
-                child: Image.asset(
-                  'images/dice$rightDiceNumber.png',
-                ),
-              ),
-            ],
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              primary: Colors.pink[200]),
-            onPressed: changeDiceFace,
-            child: const Text('Roll Dice',
-            style: TextStyle(
-              color: Color(0xff030000),
-              fontWeight: FontWeight.bold,
-            ),),
-          )
-        ],
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: MyHomePage(),
     );
   }
 }
